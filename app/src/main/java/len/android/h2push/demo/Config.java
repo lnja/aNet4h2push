eed1b967dbd05d1b76f0c7b511e4160382718e9a
lnja-s <jl9418@aliyun.com>
2019-08-19 10:28:57 +0800

    Initial commit

---
+++ app/src/main/java/len/android/h2push/demo/Config.java
@@ -0,0 +1,12 @@
+package len.android.h2push.demo;
+
+public class Config {
+
+    //API地址
+//    public static final String SERVER_HOST = "http://ip.taobao.com/";
+//    public static final String SERVER_HOST = "http://ip.360.cn/";
+    public static final String SERVER_HOST = "https://http2.akamai.com/";
+
+    //默认API分页请求大小
+    public static final int PAGE_SIZE = 10;
+}
